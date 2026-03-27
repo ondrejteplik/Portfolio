3f2030eff0bb02565538c59a13078e2a2ebaae7e
Ondřej Teplík <ondrejteplik@Ondrej--MacBook-Air-2.local>
2026-03-27 14:42:21 +0100

    Quartz sync: Mar 27, 2026, 2:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,10 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+    links: {},
   }),
 }
 
@@ -43,11 +40,6 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        { Component: Component.Darkmode() },
-      ],
-    }),
     Component.Explorer(),
   ],
   right: [],
