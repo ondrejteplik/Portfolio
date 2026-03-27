83390d4352303b6a6066dab3e9795659c8958571
Ondřej Teplík <ondrejteplik@Ondrej--MacBook-Air-2.local>
2026-03-27 14:31:31 +0100

    Quartz sync: Mar 27, 2026, 2:31 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,10 +30,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
         { Component: Component.Darkmode() },
         { Component: Component.ReaderMode() },
       ],
